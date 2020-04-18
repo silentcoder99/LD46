b01bea4641230ca39dc4425ff53f271d98a06469
Michael Smith <silentcoder99@gmail.com>
2020-04-19 08:12:33 +1000

    Organise assets

---
+++ scripts/TrashCan.gd
@@ -0,0 +1,13 @@
+extends StaticBody2D
+
+export (Texture) var trash_can_texture
+export (Texture) var fire_texture
+
+export var fire_chance = 0.1
+
+func combust():
+	$Sprite.texture = fire_texture
+
+func _on_CombustTimer_timeout():
+	if randf() < fire_chance:
+		combust()
