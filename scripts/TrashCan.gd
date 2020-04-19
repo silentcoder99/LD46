53eb0aa5b2d84de5d55f4d20666a569dfb600bc8
Michael Smith <silentcoder99@gmail.com>
2020-04-19 12:27:03 +1000

    Make repairs take time

--- scripts/TrashCan.gd
+++ scripts/TrashCan.gd
@@ -19,21 +19,30 @@ func _ready():
 	$CombustTimer.wait_time = rand_range(min_disaster_delay, max_disaster_delay)
 	$CombustTimer.start()
 
+func _process(delta):
+	$RepairBar.value = $RepairBar.max_value - ($RepairTimer.time_left / $RepairTimer.wait_time) * $RepairBar.max_value
+
 func combust():
 	on_fire = true
 	$Sprite.texture = fire_texture
 	$DisasterSound.play()
 	
-func extinguish():
-	on_fire = false
-	$Sprite.texture = trash_can_texture
-	$ResolvedSound.play()
+func repair():
+	$RepairTimer.start()
+	$RepairBar.show()
 	
 	player.money -= repair_cost
 	var tip_animation = TipAnimation.instance()
 	tip_animation.set_amount(-repair_cost)
 	tip_animation.rect_position = position + Vector2(0, -36)
 	get_tree().get_root().add_child(tip_animation)
+
+func extinguish():
+	$RepairBar.hide()
+	
+	on_fire = false
+	$Sprite.texture = trash_can_texture
+	$ResolvedSound.play()
 	
 	$CombustTimer.wait_time = rand_range(min_disaster_delay, max_disaster_delay)
 	$CombustTimer.start()
@@ -43,9 +52,9 @@ func _on_CombustTimer_timeout():
 
 func _on_FixArea_body_entered(body):
 	if "player" in body.get_groups():
-		if on_fire:
+		if on_fire and $RepairTimer.time_left == 0:
 			if player.money >= repair_cost:
-				extinguish()
+				repair()
 			else :
 				var tip_animation = TipAnimation.instance()
 				tip_animation.set_message("Not enough cash", Color(1.0, 0.0, 0.0))
@@ -55,3 +64,6 @@ func _on_FixArea_body_entered(body):
 		if on_fire:
 			body.hurt()
 			body.shoo(position.direction_to(body.position))
+
+func _on_RepairTimer_timeout():
+	extinguish()
